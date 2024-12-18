12c818f67554af62f1a4df2e0acd1c13231b9a36
Yusef Jones <yusef.jones@balfourbeatty.com>
2024-12-18 13:18:34 +0000

    Bank account classes

---
+++ C#/2 - Pair programming/QABank/SavingsAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QABank
+{
+    internal class SavingsAccount : BankAccount
+    {
+        private double InterestRate { get; set; }
+
+        public SavingsAccount(string customerName, int accountNumber, double balance, double interestRate) : base(customerName, accountNumber, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public SavingsAccount(string customerName, double balance, double interestRate) : base(customerName, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public SavingsAccount(string customerName, double interestRate) : base(customerName)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void AddInterest()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
